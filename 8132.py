b2d0c5227a08d4564b396fd04a62e0222293a959
xiaomingzhang12 <142298906+xiaomingzhang12@users.noreply.github.com>
2024-08-13 16:16:39 +0800

    Create 8132.py

---
+++ 8132.py
@@ -0,0 +1,10 @@
+# 原始列表
+numbers = [5, 3, 8, 6, 7, 2]
+
+# 升序排序
+ascending_sorted_numbers = sorted(numbers)
+print("升序排序后的列表:", ascending_sorted_numbers)
+
+# 降序排序
+descending_sorted_numbers = sorted(numbers, reverse=True)
+print("降序排序后的列表:", descending_sorted_numbers)
